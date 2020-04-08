8938fef0243d9fda64b1081621e08034007fbc8e
Luisana Pino <pino.luisana@gmail.com>
2020-04-08 10:04:05 -0400

    cambio

--- src/validator.js
+++ src/validator.js
@@ -7,7 +7,7 @@ const validator = {
         let texto = document.getElementById("tdc").value;
         let mascara = "############"+ texto.substring(12);
         //let mascara = maskify(texto);
-        //console.log("Este es el numero de tarjeta enmascarada "+ mascara);
+        console.log("Este es el numero de tarjeta enmascarada "+ mascara);
         //document.write("Tu numero de tarjeta de credito termina en " + mascara);  
         return mascara;
     },
@@ -19,43 +19,42 @@ const validator = {
           return;
         } else {
           let cantidadCaracteres = texto.length;
-          //console.log("Numero de caracteres " + cantidadCaracteres);
-          //console.log("Texto de la Cadena " + texto);
+          console.log("Numero de caracteres " + cantidadCaracteres);
+          console.log("Texto de la Cadena " + texto);
           let sum = 0;
           for (let i = cantidadCaracteres - 1; i >= 0; i--) {
             let ent = parseInt(texto.charAt(i));
-            //console.log("Entero inicial es " + ent);
-            //console.log("El indice es " + i);
+            console.log("Entero inicial es " + ent);
+            console.log("El indice es " + i);
             if (i % 2 == 0) {
                 ent = ent * 2;
-                //console.log("Es par el indice, ent * 2 es igual a " + ent);
+                console.log("Es par el indice, ent * 2 es igual a " + ent);
     
                 if (ent > 9) {
-                    //console.log("si ent es > que 9 entonces el valor es " + ent);
+                    console.log("si ent es > que 9 entonces el valor es " + ent);
                     ent = ent - 9;
-                    //console.log("Ahora ent - 9 es igual a " + ent);
+                    console.log("Ahora ent - 9 es igual a " + ent);
                 }
             }
     
             sum = sum + ent;
-            //console.log(sum);
+            console.log(sum);
           }
           let residuo = sum % 10;
-          //console.log("El residuo es " + residuo);
+          console.log("El residuo es " + residuo);
           if (residuo == 0) {
-            //console.log("Tarjeta Valida");
+            console.log("Tarjeta Valida");
             validator.maskify();
             return true;
           } else {
-            //console.log("Tarjeta InValida");
+            console.log("Tarjeta InValida");
             validator.maskify();
             return false;
           }
     
         }
       }
-
-
-};
-
-export default validator;
+  
+  };
+  
+  export default validator;
