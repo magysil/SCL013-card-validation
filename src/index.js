88059698a944059bfd376b8939600b89ab66c19d
magysil <62733263+magysil@users.noreply.github.com>
2020-04-22 19:44:09 -0400

    aaa

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ let validacionDeTarjeta = () => {
     document.getElementById("contenedor2").style.display = "none";
     document.getElementById("contenedor3").style.display = "";
     document.getElementById("mensajeDeTarjeta").innerHTML = "¡Tu tarjeta numero " + validator.maskify(creditCardNumber) +
-      " fue validada con éxito. Felicidades, ya puedes empezar a disfrutar de nuetro servicio!";
+      " fue validada con éxito. Felicidades, ya puedes empezar a disfrutar de nuestro servicio!";
     document.getElementById("nombre").value = "";
     document.getElementById("apellido").value = "";
     document.getElementById("tdc").value = "";
@@ -38,11 +38,6 @@ let validacionDeTarjeta = () => {
 }
 };
 document.getElementById("validarTdc").addEventListener("click", validacionDeTarjeta, false);
-
-
-
-
-
 
 //Retorna al Inicio
 let volverPrincipio = () => {
